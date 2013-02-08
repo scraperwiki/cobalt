93bf2b2ed2f581a54eb9901ea7b821293a25edd6
ehg <chris.blower@jumpwire.co.uk>
2013-02-08 09:18:08 +0000

    don't create top level git repo

--- code/box_lib.sh
+++ code/box_lib.sh
@@ -67,12 +67,6 @@ furnish_as_user() {
   # README.md
   sh $TEMPLATES/README.md.template $BOXNAME > README.md
 
-  # Initiate git repository.
-  git init .
-  sh $TEMPLATES/gitignore.template > .gitignore
-  git add README.md .gitignore
-  git commit -m "Box created" --author="Scraperwiki <developers@scraperwiki.com>"
-
   # create public http directory
   mkdir http
   
