81dfa3edcb0a6095bc03923e12cd32705334f722
Peter Waller <p@pwaller.net>
2014-06-09 13:01:40 +0100

    WIP: Rewrite of cgi endpoint

--- go/daemons/cgi-endpoint/main.go
+++ go/daemons/cgi-endpoint/main.go
@@ -1,93 +1,125 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"net/http/cgi"
-	"net/http/fcgi"
 	"os"
 	"os/exec"
-	"os/signal"
+	"path"
 	"strings"
+	"time"
+
+	// "net/http/cgi"
+	"github.com/scraperwiki/cobalt/go/daemons/cgi-endpoint/go/cgi"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/graceful"
 )
 
 var (
-	socketPath  = flag.String("socketpath", "/var/run/gobalt.socket", "path to listening socket")
-	socketUser  = flag.String("socketuser", "www-data", "owner for socket")
-	socketGroup = flag.String("socketgroup", "www-data", "owner for socket")
+	cobaltHome = "/var/lib/cobalt/home" // Location of boxes outside chroot
+	boxHome    = "/home"                // Location of $HOME inside one chroot
+	globalCGI  = "/tools/global-cgi"    // Location of global CGI scripts
 )
 
-type Handler struct {
+func init() {
+	if os.Getenv("COBALT_HOME") != "" {
+		cobaltHome = os.Getenv("COBALT_HOME")
+	}
+	if os.Getenv("COBALT_BOX_HOME") != "" {
+		boxHome = os.Getenv("COBALT_BOX_HOME")
+	}
+	if os.Getenv("COBALT_GLOBAL_CGI") != "" {
+		globalCGI = os.Getenv("COBALT_GLOBAL_CGI")
+	}
 }
 
-func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	log.Println(req.URL.String())
-	url := req.URL
-	path := url.Path
-
-	slice := strings.Split(path, "/")
-	filepath := strings.Join(slice[4:], "/")
-	// We either execute "sh -c something" (when not root),
-	// or "su -c something" (when root).
-	cgipath := "sh"
-	user, ok := req.Header["Cobalt-User"]
-	if !ok {
-		user = []string{"databox"}
-	}
-
-	// Prevent access via /listing
-	if slice[3] != "http" && slice[3] != "cgi-bin" {
-		http.NotFound(rw, req)
-		return
-	}
-
-	// If path begins with /http, then serve a listing.
-	if strings.HasSuffix(path, "/") && slice[3] == "http" {
-		prefix := strings.Join(slice[:4], "/")
-		root := http.Dir("/ebs/home/" + user[0] + "/http/")
-
-		staticHandler := http.StripPrefix(prefix, http.FileServer(root))
-		staticHandler.ServeHTTP(rw, req)
-		return
-	}
-	// This setup prevents shell injection.
-	// Try first /home/cgi-bin, then /home/tool/cgi-bin
-	code := `
-		if [ -x /home/cgi-bin/"$1" ] && [ ! -d /home/cgi-bin/"$1" ]; then
-			cd /home/cgi-bin && /home/cgi-bin/"$1"
-		elif [ -x /home/tool/cgi-bin/"$1" ] && [ ! -d /home/tool/cgi-bin/"$1" ]; then
-			cd /home/tool/cgi-bin && /home/tool/cgi-bin/"$1"
-		else
-			P="$1"
-
-			while :
-			do
-				# echo "Searching for $P/default"
-				if [ -x "/home/cgi-bin/$P/default" ]; then
-					# echo Found $P/default
-					cd /home/cgi-bin && /home/cgi-bin/"$P"/default
-					exit 0
-				fi
-				if [ -x "/home/tool/cgi-bin/$P/default" ]; then
-					# echo Found $P/default
-					cd /home/tool/cgi-bin && /home/tool/cgi-bin/"$P"/default
-					exit 0
-				fi
-				if [ "$P" = "." ]; then
-					break
-				fi
-				P="$(dirname "$P")"
-
-			done
-
-			echo Status: 404 Not Found
-			echo
-			echo 404 Not Found
+// This setup prevents shell injection.
+// Try first /home/cgi-bin, then /home/tool/cgi-bin
+const code = `
+	set -x
+	try_invoke() {
+		local dir="$1"
+		local bin="$2"
+		local fullpath="${dir}/${bin}"
+		if [ -x "${fullpath}" ] && [ ! -d "${fullpath}" ]; then
+			cd "${dir}" && SCRIPT_PATH="${fullpath}" exec "${fullpath}"
+			exit
 		fi
-	`
-	cgiargs := []string{"-c", code, user[0], "--", "-", filepath}
+	}
+
+	# TODO(pwaller): set SCRIPT_FILENAME correctly
+	#invoke() {
+		#SCRIPT_FILENAME="$1" "$1"
+	#}
+	# printf ".%s.\n" "$@" 1>&2
+	# echo "$@"
+
+	try_invoke "${COBALT_BOX_HOME}/cgi-bin" "$1"
+	try_invoke "${COBALT_BOX_HOME}/tool/cgi-bin" "$1"
+	try_invoke "${COBALT_GLOBAL_CGI}/" "$1"
+
+	P="$1"
+
+	while :
+	do
+		try_invoke "${COBALT_BOX_HOME}/cgi-bin" "${P}/default"
+		try_invoke "${COBALT_BOX_HOME}/tool/cgi-bin" "${P}/default"
+		# if [ -x "${COBALT_BOX_HOME}/cgi-bin/$P/default" ]; then
+		# 	cd ${COBALT_BOX_HOME}/cgi-bin && ${COBALT_BOX_HOME}/cgi-bin/"$P"/default
+		# 	exit 0
+		# fi
+		# if [ -x "${COBALT_BOX_HOME}/tool/cgi-bin/$P/default" ]; then
+		# 	cd ${COBALT_BOX_HOME}/tool/cgi-bin && ${COBALT_BOX_HOME}/tool/cgi-bin/"$P"/default
+		# 	exit 0
+		# fi
+		if [ "$P" = "." ]; then
+			break
+		fi
+		P="$(dirname "$P")"
+
+	done
+
+	echo Status: 404 Not Found
+	echo Content-Type: text/plain
+	echo
+	echo 404 Not Found
+`
+
+func GetTarget(r *http.Request) (prefix, target string) {
+	result := strings.SplitAfterN(r.URL.Path, "/", 5)
+	if len(result) != 5 {
+		log.Panic("Request URI not of the right form: %q", r.URL.RequestURI())
+	}
+	prefix = strings.Join(result[:4], "/")
+	target = result[4]
+	log.Println("TARGET: ", target)
+	return
+}
+
+func HandleHTTP(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user := vars["box"]
+
+	prefix, _ := GetTarget(r)
+
+	root := http.Dir(path.Join(cobaltHome, user, "http"))
+
+	staticHandler := http.StripPrefix(prefix, http.FileServer(root))
+	staticHandler.ServeHTTP(w, r)
+}
+
+func HandleCGI(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user := vars["box"]
+
+	_, target := GetTarget(r)
+
+	cgipath := "sh"
+	cgiargs := []string{"-c", code, user, target}
 	if os.Getuid() == 0 {
 		cgipath = "su"
 	}
@@ -103,40 +135,60 @@ func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	// on $HOME: The cgi module only sets certain
 	// environment variables, and leaves HOME unset.
 	// We set the directory within the command.
+
+	if boxHome == cobaltHome {
+		// TODO(pwaller)
+		// If they're the same, we're probably not in a chroot.
+		boxHome = path.Join(boxHome, user)
+	}
 	handler := &cgi.Handler{
 		Path: cgipath,
 		Args: cgiargs,
 		Env: []string{
-			"SCRIPT_NAME=" + path,
-			"SCRIPT_FILENAME=" + "/home/cgi-bin/" + filepath,
-			"SERVER_SOFTWARE=github.com/scraperwiki/gobalt",
+			// "SCRIPT_FILENAME=" + "/home/cgi-bin/" + target,
+			"SERVER_SOFTWARE=github.com/scraperwiki/cobalt/go/daemons/cgi-endpoint",
+			"COBALT_BOX_HOME=" + boxHome,
 		}}
-	handler.ServeHTTP(rw, req)
+
+	handler.ServeHTTP(w, r)
+}
+
+func Listen(host, port string) (l net.Listener, err error) {
+	if len(port) == 0 {
+		err = fmt.Errorf("Bad listen address, please set PORT and optionally HOST")
+		return
+	}
+	if host == "unix" {
+		l, err = net.Listen("unix", port)
+	} else {
+		addr := host + ":" + port
+		l, err = net.Listen("tcp", addr)
+	}
+	return
 }
 
 func main() {
-	flag.Parse()
-	_ = os.Remove(*socketPath)
-	l, err := net.Listen("unix", *socketPath)
+
+	l, err := Listen(os.Getenv("HOST"), os.Getenv("PORT"))
 	if err != nil {
-		log.Panic(err)
+		log.Fatalln("Error listening:", err)
 	}
 	defer l.Close()
-	err = exec.Command("chown", *socketUser+":"+*socketGroup, *socketPath).Run()
-	if err != nil {
-		log.Panic(err)
+
+	router := mux.NewRouter()
+
+	box := router.PathPrefix("/{box}/{publishToken}/").Subrouter()
+
+	box.PathPrefix("/cgi-bin/").HandlerFunc(HandleCGI)
+	box.PathPrefix("/http/").HandlerFunc(HandleHTTP)
+
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	s := &http.Server{
+		Handler: n,
 	}
-	handler := &Handler{}
-	log.Println("Serving on", *socketPath)
 
-	go func() {
-		err = fcgi.Serve(l, handler)
-		if err != nil {
-			log.Panic(err)
-		}
-	}()
-
-	sig := make(chan os.Signal)
-	signal.Notify(sig, os.Interrupt)
-	<-sig
+	const gracefulShutdownTime = 5 * time.Second
+	graceful.Serve(s, l, gracefulShutdownTime)
 }
