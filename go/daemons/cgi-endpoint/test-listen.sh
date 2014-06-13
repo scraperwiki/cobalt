5aba6dd9ed6fb580192b0a5fc13fce9aaa90d2dc
Peter Waller <peter@scraperwiki.com>
2014-06-13 16:36:46 +0100

    Update fixtures

--- go/daemons/cgi-endpoint/test-listen.sh
+++ go/daemons/cgi-endpoint/test-listen.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 
+FIXTURES=$PWD/fixtures
+
 go build &&
 
-PORT=3000 COBALT_HOME=$PWD/homes COBALT_BOX_HOME=$PWD/homes COBALT_GLOBAL_CGI=$PWD/global ./cgi-endpoint
+PORT=3000 \
+COBALT_HOME=$FIXTURES/homes \
+COBALT_BOX_HOME=$FIXTURES/homes \
+COBALT_GLOBAL_CGI=$FIXTURES/global \
+./cgi-endpoint
