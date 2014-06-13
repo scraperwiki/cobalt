ac93ef604fda1293e22f7599e82f1d9950144219
Peter Waller <peter@scraperwiki.com>
2014-06-13 16:00:36 +0100

    Remove shell wrapper

--- go/daemons/cgi-endpoint/main.go
+++ go/daemons/cgi-endpoint/main.go
@@ -41,89 +41,15 @@ func init() {
 	inProduction = os.Getenv("SCRAPERWIKI_ENV") == "production"
 }
 
-// This setup prevents shell injection.
-const code = `
-	# set -x
-
-	# Children don't inherit these.
-	declare +x COBALT_HOME COBALT_BOX_HOME COBALT_GLOBAL_CGI SCRAPERWIKI_ENV
-
-	# Intentionally inherited in case anyone wants to know.
-	declare -x COBALT_URI_BASE
-
-	# This code runs inside a chroot in production environments.
-
-	# Try to invoke the specified CGI script if it exists, is executable,
-	# is not a directory. On success, exit this shell script immediately.
-
-	try_invoke() {
-		local base="$1"
-		local dir="$2"
-		local bin="$3"
-
-		# Path with respect to http
-		local uri="${dir}/${bin}"
-
-		# Path on disk (inside chroot)
-		local fullpath="${base}${uri}"
-
-		if [ -x "${fullpath}" ] && [ ! -d "${fullpath}" ]; then
-			export SCRIPT_NAME="${COBALT_URI_BASE}${uri}" SCRIPT_PATH="${fullpath}"
-			cd "${base}/${dir}" && exec "${fullpath}"
-			exit
-		fi
-	}
-
-	try_cgi() {
-		local base="$1"
-		local dir="$2"
-		local target="$3"
-
-		try_invoke "${base}" "${dir}" "${target}"
-
-		local test_path="${target}"
-
-		# Progressively strip basename from ${test_path}, searching for a
-		# .../default script that can be invoked.
-		while :
-		do
-			try_invoke "${base}" "${dir}" "${test_path}/default"
-
-			if [ "${test_path}" = "." ]; then
-				break
-			fi
-			local test_path="$(dirname "${test_path}")"
-		done
-	}
-
-	# Try user's /home/cgi-bin, then user's /home/tool/cgi-bin, then
-	# finally the global ${COBALT_GLOBAL_CGI}/cgi-bin.
-
-	# For each case, first try directly invoking it, then try and see if
-	# a ./default script inside a directory should be ran.
-
-	# The first hit to succeed causes this script to immediately exit.
-
-	try_cgi "${COBALT_BOX_HOME}" "/cgi-bin" "$1"
-	try_cgi "${COBALT_BOX_HOME}" "/tool/cgi-bin" "$1"
-	try_cgi "${COBALT_GLOBAL_CGI}" "/cgi-bin" "$1"
-
-	echo Status: 404 Not Found
-	echo Content-Type: text/plain
-	echo
-	echo 404 Not Found
-`
-
 // Split the string into the bit which identifies the box and task
 // ({boxname}, {publishToken}, {task e.g cgi-bin}) and (script path to invoke)
 func GetTarget(r *http.Request) (prefix, target string) {
-	result := strings.SplitAfterN(r.URL.Path, "/", 5)
+	result := strings.SplitN(r.URL.Path, "/", 5)
 	if len(result) != 5 {
 		log.Panic("Request URI not of the right form: %q", r.URL.RequestURI())
 	}
 	prefix = strings.Join(result[:4], "/")
-	target = result[4]
-	log.Println("TARGET: ", target)
+	target = "/" + result[4]
 	return
 }
 
@@ -134,23 +60,114 @@ func HandleHTTP(w http.ResponseWriter, r *http.Request) {
 	prefix, _ := GetTarget(r)
 
 	root := http.Dir(path.Join(cobaltHome, user, "http"))
-
 	staticHandler := http.StripPrefix(prefix, http.FileServer(root))
 	staticHandler.ServeHTTP(w, r)
+}
+
+// Note: this assumes running in a context with the UID or GID of the
+// user/group of the file. This is good enough for us.
+// In an ideal world the file would just be invoked, but it's not possible
+// to do this here, since we don't want to run the whole CGI handler repeatedly
+// for every possible file, since it's rather expensive.
+func isExecutable(mode os.FileMode) bool {
+	return int(mode&0111) != 0
+}
+
+func Exists(path string) bool {
+	// log.Println("Tried", path)
+	s, err := os.Stat(path)
+	if err == nil && !s.IsDir() {
+		return isExecutable(s.Mode())
+	}
+	return false
+}
+
+// Locate `target` in `users` box, returning the resulting path and true if
+// found. It also searches for matching default scripts, and in the
+// /tool/cgi-bin directory, and in the /tool/globals directory.
+func FindCgiScript(user, target string) (fullpath, uri string, ok bool) {
+	var thisBoxHome = boxHome
+	if !inProduction {
+		// If we're in a production environment, then we find home at `boxHome`,
+		// otherwise it's at `{boxHome}/{username}`.
+		thisBoxHome = path.Join(thisBoxHome, user)
+	}
+
+	// Search for a script at {root}/{target}, then look for matching scripts
+	// living in directories named `default`.
+	// Returns the path to the script to invoke relative to `root` when found,
+	// which may be a script named `default`.
+	lookForScript := func(root, target string) (uri string, ok bool) {
+		ok = Exists(path.Join(root, target))
+		if ok {
+			return target, true
+		}
+		for target != "." {
+			uri := path.Join(target, "default")
+			ok = Exists(path.Join(root, uri))
+			if ok {
+				return uri, true
+			}
+			target = path.Dir(target)
+		}
+		return "", false
+	}
+
+	roots := []struct {
+		base, place string
+	}{
+		{thisBoxHome, "cgi-bin"},
+		{thisBoxHome, "tool/cgi-bin"},
+		{globalCGI, "cgi-bin"},
+	}
+
+	for _, root := range roots {
+		uri, ok := lookForScript(root.base, path.Join(root.place, target))
+		if ok {
+			return path.Join(root.base, uri), uri, ok
+		}
+	}
+
+	return "", "", false
 }
 
 func HandleCGI(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	user := vars["box"]
+	prefix, target := GetTarget(r)
 
-	_, target := GetTarget(r)
+	scriptPath, scriptUri, ok := FindCgiScript(user, target)
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+	log.Printf("%v = FindCgiScript(%v) -> %v", ok, target, scriptPath)
 
-	cgipath := "sh"
-	cgiargs := []string{"-c", code, user, target}
+	var cgipath = ""
+	var cgiargs = []string{}
+
+	// We have to use shell because we're using su to become the right user.
+	// We also can't specify these varibles directly because they're
+	// overwritten by the CGI handler.
+	const code = `
+		# SCRIPT_NAME is the URI to the script itself (may be equal to the
+		# location of "default")
+		export SCRIPT_NAME="$1"; shift
+		# These two are the full path of the script being invoked
+		export SCRIPT_FILENAME="$1"
+		export SCRIPT_PATH="$1"
+		exec "$@"
+	`
+
+	args := []string{path.Join(prefix, scriptUri), scriptPath}
 
 	if inProduction {
 		cgipath = "/bin/su"
-		cgiargs = append([]string{"--shell=/bin/bash"}, cgiargs...)
+		cgiargs = append([]string{"-c", code, user, "--", "-sh"}, args...)
+	} else {
+		// sh doesn't take `user`, nor `$0`.
+		cgipath = "/bin/sh"
+		cgiargs = append([]string{"-c", code, "--"}, args...)
 	}
 
 	var thisBoxHome = boxHome
@@ -169,10 +186,7 @@ func HandleCGI(w http.ResponseWriter, r *http.Request) {
 		Path: cgipath,
 		Args: cgiargs,
 		Env: []string{
-			fmt.Sprint("COBALT_URI_BASE=/", vars["box"], "/", vars["publishToken"]),
 			"SERVER_SOFTWARE=github.com/scraperwiki/cobalt/go/daemons/cgi-endpoint",
-			"COBALT_BOX_HOME=" + thisBoxHome,
-			"COBALT_GLOBAL_CGI=" + globalCGI,
 		}}
 
 	handler.ServeHTTP(w, r)
